f96832cd0a631f169a8be27f19e02c1c09b82e93
Aarón Parres López <aaronplmultimedia@gmail.com>
2021-07-25 20:18:08 +0200

    Add toggle buttons for open/closed issues

--- src/components/IssuesList/index.tsx
+++ src/components/IssuesList/index.tsx
@@ -1,15 +1,50 @@
-import { useGetRepoIssuesQuery } from 'hooks/apihooks';
+import { IssueState, useGetRepoIssuesLazyQuery } from 'hooks/apihooks';
+import { useAppDispatch, useAppSelector } from 'hooks/redux';
+import { useEffect } from 'react';
 import { Link } from 'react-router-dom';
+import {
+	selectListIssueType,
+	toggleListIssuesType,
+} from 'store/slices/settings';
 
 import IssueItem from './IssueItem';
+import './styles.scss';
 
 export default function IssuesList() {
-	const { data, error, loading } = useGetRepoIssuesQuery({
-		variables: { name: 'react', owner: 'facebook' },
-	});
+	const dispatch = useAppDispatch();
+	const listIssueType = useAppSelector(selectListIssueType);
+	const [getRepoIssues, { data, error, loading }] = useGetRepoIssuesLazyQuery();
+
+	useEffect(() => {
+		getRepoIssues({
+			variables: { name: 'react', owner: 'facebook', state: listIssueType },
+		});
+	}, [listIssueType]);
+
+	const switchHandler = (state: IssueState) => {
+		dispatch(toggleListIssuesType(state));
+	};
 
 	return (
 		<div className="container">
+			<div className="buttonContainer">
+				<div
+					className={`${
+						listIssueType === IssueState.Open ? 'buttonSelected' : 'buttonIdle'
+					}`}
+				>
+					<div onClick={() => switchHandler(IssueState.Open)}>OPEN</div>
+				</div>
+				<div
+					className={`${
+						listIssueType === IssueState.Closed
+							? 'buttonSelected'
+							: 'buttonIdle'
+					}`}
+				>
+					<div onClick={() => switchHandler(IssueState.Closed)}>CLOSED</div>
+				</div>
+			</div>
 			<h1>ISSUES</h1>
 			{loading ? (
 				<>Loading...</>
