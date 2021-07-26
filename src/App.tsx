5682dcbd8f3b62d1a050b3812836a0a683c09dfe
Aarón Parres López <aaronplmultimedia@gmail.com>
2021-07-27 00:19:36 +0200

    Create new Spinner component & redux showSpinner logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
 import { Route, Switch, Redirect } from 'react-router-dom';
+import { isErrorModalShown, isLoading } from 'store/slices/settings';
+import { useAppSelector } from 'hooks/redux';
 
 import Issues from 'components/Issues';
 import Search from 'components/Search';
@@ -7,10 +9,14 @@ import Navbar from 'components/Layout/Navbar';
 import Footer from 'components/Layout/Footer';
 
 import styles from './App.module.scss';
+import Spinner from 'components/UI/Spinner';
+import Modal from 'components/UI/Modal';
 
 export default function App() {
+	const loading = useAppSelector(isLoading);
+	const error = useAppSelector(isErrorModalShown);
 	return (
-		<div style={{ width: '100%' }}>
+		<div className={styles.app}>
 			<Navbar />
 			<div className={styles.container}>
 				<Switch>
@@ -19,6 +25,8 @@ export default function App() {
 					<Route exact path="/" component={Issues} />
 					<Redirect to="/" />
 				</Switch>
+				{loading && <Spinner />}
+				{error && <Modal />}
 			</div>
 			<Footer />
 		</div>
--- src/store/slices/settings.ts
+++ src/store/slices/settings.ts
@@ -3,10 +3,14 @@ import type { RootState } from 'store/createStore';
 import { IssueState } from 'hooks/apihooks';
 
 interface settingsState {
+	loading: boolean;
+	showErrorModal: boolean;
 	listIssuesType: IssueState;
 }
 
 const initialState: settingsState = {
+	loading: false,
+	showErrorModal: false,
 	listIssuesType: IssueState.Open,
 };
 
@@ -14,6 +18,12 @@ export const settingsSlice = createSlice({
 	name: 'settings',
 	initialState: initialState,
 	reducers: {
+		changeLoadingValue: (state, action: PayloadAction<boolean>) => {
+			state.loading = action.payload;
+		},
+		showErrorModal: (state, action: PayloadAction<boolean>) => {
+			state.showErrorModal = action.payload;
+		},
 		toggleListIssuesType: (state, action: PayloadAction<IssueState>) => {
 			state.listIssuesType = action.payload;
 		},
@@ -21,10 +31,16 @@ export const settingsSlice = createSlice({
 });
 
 // SELECTORS
+export const isLoading = (state: RootState) => state.settings.loading;
+
+export const isErrorModalShown = (state: RootState) =>
+	state.settings.showErrorModal;
+
 export const selectListIssueType = (state: RootState) =>
 	state.settings.listIssuesType;
 
 // ACTIONS
-export const { toggleListIssuesType } = settingsSlice.actions;
+export const { changeLoadingValue, showErrorModal, toggleListIssuesType } =
+	settingsSlice.actions;
 
 export default settingsSlice.reducer;
